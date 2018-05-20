1a2dbe11e34f39b785c711d250e0e26a065870c8
jbirdx <keixxxxxxxx@gmail.com>
2018-05-20 23:42:59 +0900

    Initial commit

---
+++ app/src/main/java/myapps/dentaku/MainActivity.java
@@ -0,0 +1,23 @@
+package myapps.dentaku;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        //Option + Enter
+        TextView textView = (TextView) findViewById(R.id.textview);
+        textView.setText("こんにちは！ Android Studio");
+//        textView.setText(String.valueOf(42));
+
+
+
+    }
+}
